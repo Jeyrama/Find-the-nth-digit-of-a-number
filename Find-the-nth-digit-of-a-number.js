e345b361ca5f937a1a8b1c1d44119ad3c7641acd
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2023-06-22 10:20:42 -0700

    Update Find-the-nth-digit-of-a-number.js

--- Find-the-nth-digit-of-a-number.js
+++ Find-the-nth-digit-of-a-number.js
@@ -17,3 +17,14 @@ Examples(num, nth --> output)
 
 
 // Solution
+
+let findDigit = function(num, nth) {
+  if(nth <= 0)
+    return -1;
+
+  let x = Math.abs(num);
+  for (let i = 1; i < nth; i++) {
+    x = Math.floor(x/10);
+  }
+  return x%10;
+}
